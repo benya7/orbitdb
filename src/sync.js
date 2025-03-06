b04b6e4d6852c5ce943d563b5172c3d7929f90a6
benya7 <bitup.games@gmail.com>
2025-03-06 16:43:20 +0100

    update: runOnLimitedConnection option

--- src/sync.js
+++ src/sync.js
@@ -196,7 +196,7 @@ const Sync = async ({ ipfs, log, events, onSynced, start, timeout }) => {
           for (let attempt = 1; attempt <= maxRetries; attempt++) {
             try {
             console.log(`Peer ${peerId} subscribed to ${address}, dialing. attempt ${attempt}/${maxRetries}`)
-              stream = await libp2p.dialProtocol(remotePeer, headsSyncAddress, { signal })
+              stream = await libp2p.dialProtocol(remotePeer, headsSyncAddress, { signal, runOnLimitedConnection: true })
               console.log(`Successfully dialed ${peerId} on attempt ${attempt}`)
               break
             } catch (e) {
@@ -291,7 +291,7 @@ const Sync = async ({ ipfs, log, events, onSynced, start, timeout }) => {
   const startSync = async () => {
     if (!started) {
       // Exchange head entries with peers when connected
-      await libp2p.handle(headsSyncAddress, handleReceiveHeads)
+      await libp2p.handle(headsSyncAddress, handleReceiveHeads, { runOnLimitedConnection: true })
       pubsub.addEventListener('subscription-change', handlePeerSubscribed)
       pubsub.addEventListener('message', handleUpdateMessage)
       // Subscribe to the pubsub channel for this database through which updates are sent
